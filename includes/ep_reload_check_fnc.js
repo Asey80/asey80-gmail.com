4c71c4e0c85c822b93eb1575ff79faa2db53ceab
Oleksii Asieiev <google-oauth2|116681228463055200889>
2021-04-02 16:38:51 +0000

    simple incremental insert

---
+++ includes/ep_reload_check_fnc.js
@@ -0,0 +1,16 @@
+function reloadCheck(dates, outlets, tbl) {
+    if (dates || outlets)   {
+        return `where S.FILE_DATE_ID IN (${dates}) and S.FILE_OUTLET_ID in (${outlets})`}
+
+    else if (outlet_ids) {
+        return `where S.FILE_OUTLET_ID in (${outlet_ids}) `}
+    
+    else if (dates) {
+        return `where S.FILE_DATE_ID IN (${dates}) `}
+        
+    else {
+        return `where S.FILE_DATE_ID > (select max(DATE_ID) from ${tbl})`}
+    
+} 
+
+module.exports = { reloadCheck };
