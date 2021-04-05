e59f4056bded1b1bfc25cf3b6dcd1eb7122899e3
Oleksii Asieiev <google-oauth2|116681228463055200889>
2021-04-05 15:00:11 +0000

    added export logic

--- includes/ep_reload_check_fnc.js
+++ includes/ep_reload_check_fnc.js
@@ -1,9 +1,6 @@
 function reloadCheck(dates, outlets, tbl) {
     if (dates && outlets)   {
         return `where S.FILE_DATE_ID IN (${dates}) and S.FILE_OUTLET_ID in (${outlets})`}
-
-    else if (outlet_ids) {
-        return `where S.FILE_OUTLET_ID in (${outlet_ids}) `}
     
     else if (dates) {
         return `where S.FILE_DATE_ID IN (${dates}) `}
